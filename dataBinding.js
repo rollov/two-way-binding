2b39c8cca07adc92b85ff4cdab34454ddd5cc415
Rolf Richter <ror@spreadshirt.net>
2013-11-22 13:31:14 +0100

    add code to replace {value} placehoders in html tag attributes

--- dataBinding.js
+++ dataBinding.js
@@ -1,17 +1,31 @@
 (function (window) {
     window.DataBinding = window.DataBinding || {};
 
-    var Binder = function (object) {
+    var Binder = function (options) {
         var publicMethodes,
             callbacks = {},
-            objectId = object.id,
+            objectId = options.id,
             self = this;
 
         function bindEvents (initiator) {
             var elements = document.querySelectorAll("[data-bind-" + objectId + "]");
 
-            for (var $i = 0, $l = elements.length; $i < $l; $i++) {
-                elements[$i].addEventListener('keyup', function (event) {
+            for (var i = 0, l = elements.length; i < l; i++) {
+
+                // add property and method to check focus
+                elements[i].focused = false;
+                elements[i].hasFocus = function () {
+                   return this.focused;
+                };
+                elements[i].onfocus = function () {
+                   this.focused = true;
+                };
+                elements[i].blur = function () {
+                   this.focused = false;
+                };
+
+                // add event listener
+                elements[i].addEventListener('keyup', function (event) {
                     publicMethodes.publish(this.getAttribute('data-bind-' + objectId), this.value, initiator);
                 });
             }
@@ -19,6 +33,7 @@
 
         publicMethodes = {
             publish: function (prop, value, initiator, callback) {
+
                 // cache callbacks if passed
                 if (callback)
                     callbacks[prop] = callback;
@@ -27,15 +42,29 @@
                 var elements = document.querySelectorAll("[data-bind-" + objectId + "=" + prop + "]");
 
                 // loop elements and set value
-                for (var $i = 0, $l = elements.length; $i < $l; $i++) {
-                    if (elements[$i].tagName == 'INPUT') {
-                        elements[$i].value = value;
-                    } else {
-                        elements[$i].innerHTML = value;
+                for (var i = 0, l = elements.length; i < l; i++) {
+
+                    // loop attributes and replace val placeholder
+                    for (var a = elements[i].attributes.length - 1 ; a >= 0; a-- ) {
+                        var attr = elements[i].attributes[a].value;
+                        if (attr.match(/{value}/)) {
+                            attr = attr.replace(/{value}/g, value);
+                            elements[i].attributes[a].value = attr;
+                        }
                     }
 
+                    if (elements[i].toString().match(/value/))
+                       console.log(elements[i].toString().replace(/{value}/, value));
+
+                    if (elements[i].hasFocus() == false)
+                        if (elements[i].tagName == 'INPUT') {
+                            elements[i].value = value;
+                        } else {
+                            elements[i].innerHTML = value;
+                        }
+
                     if (initiator == self)
-                        object[prop] = value;
+                        options.data[prop] = value;
                 }
 
                 callbacks[prop] && callbacks[prop](value);
@@ -46,8 +75,8 @@
         return publicMethodes;
     };
 
-    DataBinding.create = function (object, callback) {
-        var instance = new Binder(object);
+    DataBinding.create = function (options, callback) {
+        var instance = new Binder(options);
         callback && callback();
         return instance
     };
